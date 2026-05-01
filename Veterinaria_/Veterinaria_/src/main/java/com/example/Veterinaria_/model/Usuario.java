95b63c34c017b5e5b903aa92aef05c59c1dde526
Pavlo <pablosotog2006@gmail.com>
2026-05-01 02:24:36 -0400

    Solucionando problema de carpetas vacías y agregando contenido

--- Veterinaria_/Veterinaria_/src/main/java/com/example/Veterinaria_/model/Usuario.java
+++ Veterinaria_/Veterinaria_/src/main/java/com/example/Veterinaria_/model/Usuario.java
@@ -1,5 +1,31 @@
 package com.example.Veterinaria_.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name="Usuario")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+    private String nombre;
+    private String rut;
+    private String email;
+    private String telefono;
+    private String Direccion;
+
 
 }
