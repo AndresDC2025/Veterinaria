9e0de68e62b30d1cd0da41eeda5dc2c26c990156
Pavlo <pablosotog2006@gmail.com>
2026-05-01 01:26:42 -0400

    Inicio del Proyecto

--- Veterinaria_/Veterinaria_/src/main/java/com/example/Veterinaria_/VeterinariaApplication.java
+++ Veterinaria_/Veterinaria_/src/main/java/com/example/Veterinaria_/VeterinariaApplication.java
@@ -3,6 +3,13 @@ package com.example.Veterinaria_;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+/*	
+
+ESTE ES EL INICO DEL PROYECTO.
+
+*/
 @SpringBootApplication
 public class VeterinariaApplication {
 
