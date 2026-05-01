3d72668e9ce2f4592c8fdde1f0384cd387a356d9
Pavlo <pablosotog2006@gmail.com>
2026-05-01 12:14:19 -0400

    01/05/2026

--- Veterinaria_/Veterinaria_/src/main/java/com/example/Veterinaria_/VeterinariaApplication.java
+++ Veterinaria_/Veterinaria_/src/main/java/com/example/Veterinaria_/VeterinariaApplication.java
@@ -15,10 +15,7 @@ public class VeterinariaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VeterinariaApplication.class, args);
-<<<<<<< HEAD
+
 	}
-<<<<<<< HEAD
-	/* 
-	Dg tay entera rica  test test test test test test test test test 55555555tttt
-	*/
+	
 }
