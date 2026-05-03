b6937b602e4bc66c55299139a26d433536b702df
Pavlo <pablosotog2006@gmail.com>
2026-05-02 21:27:45 -0400

    02/05/2026 | MODELO DE MASCOTAS LISTO

---
+++ Mascotas/mascotas/src/main/java/com/example/mascotas/model/Mascota.java
@@ -0,0 +1,28 @@
+package com.example.mascotas.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "mascota")
+public class Mascota {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String raza;
+    private int edad;
+    private String nacimiento;
+
+
+}
