81c03935934d8a2b7f3bb461827840928463747b
Pavlo <pablosotog2006@gmail.com>
2026-05-10 23:16:08 -0400

    10/05/2026 | Avance de Tokens y logs

---
+++ Mascotas/mascotas/src/main/java/com/example/mascotas/dto/MascotaDTO.java
@@ -0,0 +1,19 @@
+package com.example.mascotas.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+public class MascotaDTO {
+
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    @NotBlank(message = " es obligatorio")
+    private String raza;
+
+    @NotNull(message = "La edad es obligatoria")
+    @Min(value = 0, message = "La edad debe ser positiva")
+    private int edad;
+
+}
