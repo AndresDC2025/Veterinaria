f343743d59e0dffcbadddbb56b4ff0af9a861adb
Pavlo <pablosotog2006@gmail.com>
2026-05-14 01:09:43 -0400

    13/05/2026 | Beta 2

--- Historial_Medico/Historial_Medico/src/main/resources/db/migration/V1__historial_medico.sql
+++ Historial_Medico/Historial_Medico/src/main/resources/db/migration/V1__historial_medico.sql
@@ -1,10 +1,12 @@
 CREATE TABLE historial_medico (
-    id integer NOT NULL AUTO_INCREMENT,
+
+    id INTEGER NOT NULL AUTO_INCREMENT,
     diagnostico VARCHAR(255),
     tratamiento VARCHAR(255),
     notas VARCHAR(255),
-    idMascota integer,
+    idMascota INTEGER,
     /* fecha DATE, */
-    PRIMARY KEY (id),
 
-)
+    PRIMARY KEY (id)
+
+);
